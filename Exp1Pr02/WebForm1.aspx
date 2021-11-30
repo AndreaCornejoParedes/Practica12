f661ef856592c4a9de6d8e398fdf49d4e377bb11
AndreaCornejoParedes <74048310@ucsm.edu.pe>
2021-11-30 08:54:19 -0500

    Experiencia 2

---
+++ Exp1Pr02/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Exp1Pr02.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html class ="bg-black" xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Experiencia 3</title>
+  
+</head>
+<body class="bg-black">
+    
+       <form id="form1" runat="server">
+           <div>
+               <asp:ScriptManager ID="sm" runat="server">
+
+               </asp:ScriptManager>
+                <asp:Label ID="Label1" runat="server" Text="Label" Width="196px"></asp:Label>
+               
+               <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                   <ContentTemplate>
+                       <asp:Label ID="Label2" runat="server" Text="Label" Width="196px"></asp:Label>
+               <br />
+                       <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Page_Load"></asp:Timer>
+                   </ContentTemplate>
+               </asp:UpdatePanel>
+           </div>
+       </form>
+   
+</body>
+</html>
---
+++ Exp1Pr02/WebForm1.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Exp1Pr02
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control sm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager sm;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control Timer1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.Timer Timer1;
+    }
+}
